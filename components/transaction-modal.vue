891af9e1b0ddab6d76db0f82ea1e50ec2c6127ed
Vanhaeren Antonius <9069366+tonnyvanhaeren@users.noreply.github.com>
2025-01-06 17:41:17 +0100

    added form

--- components/transaction-modal.vue
+++ components/transaction-modal.vue
@@ -2,12 +2,49 @@
   <UModal v-model="isOpen">
     <UCard>
       <template #header> Add Transaction </template>
-      <div>Hello!</div>
+      <UFormGroup
+        :required="true"
+        label="Transaction Type"
+        name="type"
+        class="mb-4"
+      >
+        <USelect placeholder="Select the Transaction type" :options="types" />
+      </UFormGroup>
+      <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+        <UInput type="number" placeholder="amount" />
+      </UFormGroup>
+      <UFormGroup
+        label="Transaction date"
+        :required="true"
+        name="created_at"
+        class="mb-4"
+      >
+        <UInput type="date" picon="i-heroicons-calendar-days-20-solid" />
+      </UFormGroup>
+      <UFormGroup
+        label="Description"
+        hint="Optional"
+        name="description"
+        class="mb-4"
+      >
+        <UInput placeholder="Description" />
+      </UFormGroup>
+      <UFormGroup
+        :required="true"
+        label="Category"
+        name="category"
+        class="mb-4"
+      >
+        <USelect placeholder="Select the Category" :options="categories" />
+      </UFormGroup>
+      <UButton type="submit" color="black" variant="solid" label="Save" />
     </UCard>
   </UModal>
 </template>
 
 <script setup>
+// import { UFormGroup } from "#build/components";
+import { categories, types } from "~/constants";
 const props = defineProps({
   modelValue: Boolean,
 });
@@ -15,5 +52,13 @@ const emit = defineEmits(["update:modelValue"]);
 const isOpen = computed({
   get: () => props.modelValue,
   set: (value) => emit("update:modelValue", value),
+});
+
+const state = ref({
+  type: undefined,
+  amount: 0,
+  created_at: undefined,
+  description: undefined,
+  category: undefined,
 });
 </script>
--- constants.js
+++ constants.js
@@ -1,1 +1,4 @@
 export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+
+export const categories = ["Food", "Housing", "Car", "Entertainment"];
+export const types = ["Income", "Expense", "Saving", "Investment"];
